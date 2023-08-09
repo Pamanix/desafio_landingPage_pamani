2527b495394e398174b3f4af6af0bfbffdb92e73
Pamanix <pahrodr@gmail.com>
2023-08-09 18:16:48 -0300

    adicionando uma funcionalidade no JS para cumprir o requisito de desafio sem alterar o escolpo proposto

---
+++ index.js
@@ -0,0 +1,11 @@
+var imagem = window.document.getElementById("image");
+
+function AlterarImagem(){
+    imagem.style = "display:none"
+    imagem.style = "display:flex"
+    
+
+}
+
+/*funçao feita somente para adicionar algum uso para o JS 
+cumprindo o requisito sem mudar o projeto proposto*/
